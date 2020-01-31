5657db66544254fdea0892f196aef794ac73c3fc
vfichtner <email>
2020-01-31 22:34:22 +0100

    Initial commit

---
+++ src/main/java/com/example/sample/sampledockerjavaapp/SampleDockerJavaAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.sample.sampledockerjavaapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleDockerJavaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleDockerJavaAppApplication.class, args);
+	}
+
+}
